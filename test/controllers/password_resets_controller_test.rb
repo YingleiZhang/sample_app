1c33bb0d04627a225af9970a5f945312364ce2b7
Yinglei Zhang <yinglei.zh@gmail.com>
2015-03-25 23:39:17 -0400

    add password reset, one test failed

---
+++ test/controllers/password_resets_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class PasswordResetsControllerTest < ActionController::TestCase
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+  
+  test "should get edit" do
+    get :edit
+    assert_response :success
+  end
+end
