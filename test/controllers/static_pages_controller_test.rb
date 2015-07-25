3c1c0a84e439d4114b477efd4cda1779b14e600e
Yinglei Zhang <yinglei.zh@gmail.com>
2015-07-25 17:33:59 -0400

    change the base title of this site

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,24 +4,24 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Yinglei's Personal Blog"
   end
 
   test "should get resume" do
     get :resume
     assert_response :success
-    assert_select "title", "Resume | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Resume | Yinglei's Personal Blog"
   end
   
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Yinglei's Personal Blog"
   end
 
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | Yinglei's Personal Blog"
   end
 end
